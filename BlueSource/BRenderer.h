1030598418753097de282c63f8c706f467608282
pinegreen83 <83223050+pinegreen83@users.noreply.github.com>
2025-04-09 23:39:16 +0900

    삼각형 그리기

--- BlueSource/BRenderer.h
+++ BlueSource/BRenderer.h
@@ -1,8 +1,25 @@
 #pragma once
 #include "BCamera.h"
+#include "BGraphicDevice_DX11.h"
 
 namespace blue::renderer
 {
 	extern Camera* mainCamera;
+
+	struct Vertex
+	{
+		Vector3 pos;
+		Vector4 color;
+	};
+
+	extern Vertex vertexes[3];
+	extern ID3D11Buffer* vertexBuffer;
+
+	extern ID3DBlob* vsBlob;
+	extern ID3D11VertexShader* vsShader;
+	
+	extern ID3DBlob* psBlob;
+	extern ID3D11PixelShader* psShader;
+	extern ID3D11InputLayout* inputLayouts;
 
 }
